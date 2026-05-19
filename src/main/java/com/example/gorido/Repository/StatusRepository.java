b1dbcbbb0fd8c7a13562ea1d492831e5509a157e
IT25102285 <it25102285@my.sliit.lk>
2026-05-20 00:53:33 +0530

    Delete src directory

--- src/main/java/com/example/gorido/Repository/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gorido.Repository;
-
-import com.example.gorido.Model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
